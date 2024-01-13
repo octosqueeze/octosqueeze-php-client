bf1676b3f04ec1dad25aaf200901f80b4a2f0b56
OctoSqueeze <contact@octosqueeze.com>
2024-01-14 11:31:01 +1300

    WIP

--- src/OctoSqueeze.php
+++ src/OctoSqueeze.php
@@ -2,10 +2,259 @@
 
 namespace OctoSqueeze\Client;
 
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
 final class OctoSqueeze
 {
-    public static function create($apiKey = null)
+    /**
+     * The API key for the requests.
+     */
+    private ?string $apiKey = null;
+
+    /**
+     * The endpoint URI for the requests.
+     */
+    private ?string $endpointUri = 'https://api.octosqueeze.com';
+
+    /**
+     * The HTTP client for the requests.
+     */
+    private ?Client $httpClient = null;
+
+    /**
+     * The HTTP client config.
+     */
+    private ?array $httpClientConfig = [];
+
+
+    /**
+     * Options for the image compression.
+     */
+    protected ?array $options = [
+        'formats' => ['avif', 'jpeg'],
+        // --
+        'hash_check' => true,
+        'image_id_check' => true,
+        'quality' => 1, // 1 : smallest size, 2 : best quality
+    ];
+
+    public function __construct($apiKey)
     {
-       //
+        $this->setApiKey($apiKey);
+
+        $this->setHttpClientConfig([
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . $this->getApiKey(),
+                // 'http_errors' => true,
+            ]
+        ]);
+    }
+
+    public static function client($apiKey = null)
+    {
+        return new self($apiKey);
+    }
+
+    /**
+     * Sets the API key for the requests.
+     */
+    public function setApiKey(string $apiKey): self
+    {
+        $this->apiKey = trim($apiKey);
+
+        return $this;
+    }
+
+    /**
+     * Gets the API key.
+     */
+    public function getApiKey(): string
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * Sets the options.
+     */
+    public function setOptions(array $options): self
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
+     * Gets the options.
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    /**
+     * Sets the Endpoint URI.
+     */
+    public function setEndpointUri(string $uri): self
+    {
+        $this->endpointUri = trim($uri);
+
+        $this->initializeHttpClient(true);
+
+        return $this;
+    }
+
+    /**
+     * Gets the Endpoint URI.
+     */
+    public function getEndpointUri(): string
+    {
+        return $this->endpointUri;
+    }
+
+    /**
+     * Sets the Http Client Config.
+     */
+    public function setHttpClientConfig(array $config, $owerwrite = false): self
+    {
+        $this->httpClientConfig = $owerwrite ? $config : array_merge($this->getHttpClientConfig(), $config);
+
+        $this->initializeHttpClient(true);
+
+        return $this;
+    }
+
+    /**
+     * Gets the Http Client Config.
+     */
+    public function getHttpClientConfig(): array
+    {
+        return $this->httpClientConfig;
+    }
+
+
+    /**
+     * Initialize/Set the Http Client.
+     */
+    public function initializeHttpClient($reinitialize = false): self
+    {
+        if (!$this->httpClient || $reinitialize)
+        {
+            $this->httpClient = new Client($this->getHttpClientConfig());
+        }
+
+        return $this;
+    }
+
+    /**
+     * Gets the Http Client.
+     */
+    public function getHttpClient(): Client
+    {
+        return $this->httpClient;
+    }
+
+    public function squeezeUrl(string|array $urls)//: array
+    {
+        $this->initializeHttpClient();
+
+        $client = $this->getHttpClient();
+
+        if (is_string($urls))
+        {
+            $urls = [$urls];
+        }
+
+        // prepare array format
+        foreach ($urls as $key => $url)
+        {
+            if (is_string($url))
+            {
+                $urls[$key] = [
+                    'url' => $url,
+                ];
+            }
+        }
+
+        try {
+            $response = $client->request('POST', $this->endpoint('api/compressor/urls'), [
+                'form_params' => [
+                    'items' => $urls,
+                    'options' => $this->getOptions(),
+                ]
+            ]);
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
+        // } catch (\GuzzleHttp\Exception\ClientException $e) {
+        // // } catch (\GuzzleHttp\Exception\ServerException $e) {
+            $exception = json_decode($e->getResponse()->getBody()->getContents(), true);
+        }
+
+        if (isset($exception))
+        {
+            dd($exception);
+        }
+        else
+        {
+            $content = json_decode($response->getBody()->getContents(), true);
+
+            dd($content);
+        }
+
+
+        if ($response->getStatusCode() === 200)
+        {
+            $result = json_decode($response->getBody()->getContents(), true);
+
+            return $result;
+        }
+        else
+        {
+            throw new Exception('Something went wrong, bad response');
+        }
+    }
+
+    public function squeezeFile(string $url): array
+    {
+        $this->initializeHttpClient();
+
+        // ..
+    }
+
+    public function squeezeBuffer(string $url): array
+    {
+        $this->initializeHttpClient();
+
+        // ..
+    }
+
+    public function squeezeFolder(string $path): array
+    {
+        $this->initializeHttpClient();
+
+        // ..
+    }
+
+    public function take(string|array $bundle): Tentacles
+    {
+        $tentacles = new Tentacles($bundle, $this);
+
+        return $tentacles;
+    }
+
+    public function endpoint(string $url): string
+    {
+        $base = $this->getEndpointUri();
+
+        // remove slash at the end if presented
+        $base = substr($base, -1) == '/' ? substr($base, 0, -1) : $base;
+        // remove slash at the start if presented
+        $url = substr($url, 1) == '/' ? substr($url, 1) : $url;
+
+        $finalUrl = $base . '/' . $url;
+
+        return $finalUrl;
     }
 }
---
+++ src/Tentacles.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace OctoSqueeze\Client;
+
+use GuzzleHttp\Client;
+use Symfony\Component\Filesystem\Filesystem;
+
+class Tentacles
+{
+    private ?OctoSqueeze $octo = null;
+
+    protected ?array $source;
+
+    protected ?array $bundle;
+
+    public function __construct($source, $octo)
+    {
+        $this->source = $source;
+        $this->octo = $octo;
+
+        $this->fetchCompressions();
+    }
+
+    public function toFile($dir): null|array
+    {
+        $bundle = $this->getBundle();
+
+        if (count($bundle))
+        {
+            $fs = new Filesystem();
+
+            // ! only for dev TLS verification
+            $contextOptions = [
+                'ssl' => [
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                ]
+            ];
+
+            $completed = [];
+
+            foreach ($bundle as $file)
+            {
+                foreach ($file['compressions'] as $compression)
+                {
+                    $image = file_get_contents($compression['link'], false, stream_context_create($contextOptions));
+                    $filename = $file['name'] . '.' . $compression['format'];
+
+                    if ($image)
+                    {
+                        $path = $dir .'/'. $filename;
+                        $fs->dumpFile($path, $image);
+
+                        $completed[] = $path;
+                    }
+                }
+            }
+
+            return $completed;
+        }
+    }
+
+    public function toBuffer(): array
+    {
+        return [];
+    }
+
+    /**
+     * Sets the bundle.
+     */
+    public function setBundle(array $bundle): self
+    {
+        $this->bundle = $bundle;
+
+        return $this;
+    }
+
+    /**
+     * Gets the bundle.
+     */
+    public function getBundle(): array
+    {
+        return $this->bundle;
+    }
+    protected function fetchCompressions()
+    {
+        $this->octo->initializeHttpClient();
+
+        $client = $this->octo->getHttpClient();
+
+        try {
+            $response = $client->request('POST', $this->octo->endpoint('api/compressor/take'), [
+                'form_params' => [
+                    'items' => $this->source,
+                ]
+            ]);
+        } catch (\GuzzleHttp\Exception\RequestException $e) {
+        // } catch (\GuzzleHttp\Exception\ClientException $e) {
+        // // } catch (\GuzzleHttp\Exception\ServerException $e) {
+            $exception = json_decode($e->getResponse()->getBody()->getContents(), true);
+        }
+
+        if (isset($exception))
+        {
+            // throw new Exception('Something went wrong, bad response');
+            dd($exception);
+        }
+        else
+        {
+            if ($response->getStatusCode() === 200)
+            {
+                $result = json_decode($response->getBody()->getContents(), true);
+
+                $this->setBundle($result['images']);
+
+                return $this;
+            }
+            else
+            {
+                throw new Exception('Something went wrong, bad response');
+            }
+        }
+    }
+}
